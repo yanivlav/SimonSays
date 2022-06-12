cca18b1c8ec8f7c8c124b403d936a52adf8aff24
BarDAP <barbenshabat@gmail.comgit config --global user.name BarDAP>
2022-06-12 21:57:04 +0300

    Blue flash ImageView is working manually

--- app/src/main/java/com/example/simonsays/GameActivity.java
+++ app/src/main/java/com/example/simonsays/GameActivity.java
@@ -1,23 +1,21 @@
 package com.example.simonsays;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
     Button blueBtn, yellowBtn, redBtn, greenBtn,startBtn;
+    ImageView blueAnim;
     private int roundNumber;
     private int AIe;
     private int highScore;
@@ -33,89 +31,22 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        blueBtn = (Button) findViewById(R.id.blue_btn);
+//        blueBtn = (Button) findViewById(R.id.blue_btn);
         yellowBtn = (Button)findViewById(R.id.yellow_btn);
         redBtn = (Button)findViewById(R.id.red_btn);
         greenBtn = (Button)findViewById(R.id.green_btn);
-//        startBtn = (Button)findViewById(R.id.start_btn);
+        startBtn = (Button)findViewById(R.id.start_btn);
+        blueAnim = findViewById(R.id.blue_btn);
+        AnimationDrawable animationDrawable = (AnimationDrawable) blueAnim.getDrawable();
+//        animationDrawable.start();
+
+
         final MediaPlayer mpb = MediaPlayer.create(this,R.raw.alien_blast_in_the_earth);
         final MediaPlayer mpr = MediaPlayer.create(this,R.raw.cool_guitar_riff);
         final MediaPlayer mpy = MediaPlayer.create(this,R.raw.electric_fence_buzzing);
         final MediaPlayer mpg = MediaPlayer.create(this,R.raw.scifi_artificial_intelligence_speaks);
 
 //        Timer timer = new Timer();
-//
-//
-//
-//        int level = 1;//counter for level
-//
-////        Button[] buttons = {blueBtn,yellowBtn,redBtn,greenBtn};
-//
-//        Random rand = new Random();
-////        int randNum = rand.nextInt(3);
-//
-//        ArrayList<String> first = new ArrayList<>();
-
-//        first.add(colors[randNum]);
-
-//        //initialize 100 first colors
-//        for (int i = 0; i < 100 ; i++){
-//            first.add(colors[rand.nextInt(3)]);
-//
-//        }
-//        for (int i = 0 ; i < first.size(); i++){
-//                switch(first.get(i)) {
-//                    case "Blue":
-//                        mpb.start();
-//                        break;
-//                    case "Red":
-//                        mpr.start();
-//                        break;
-//                    case "Green":
-//                        mpg.start();
-//                        break;
-//                    case "Yellow":
-//                        mpy.start();
-//                        break;
-//                    default:
-//                        // code block
-//                }
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//        }
-
-//        while (level < 100){
-//            for (int i = 0 ; i < level; i++){
-//                switch(first.get(i)) {
-//                    case "Blue":
-//                        mpb.start();
-//                        break;
-//                    case "Red":
-//                        mpr.start();
-//                        break;
-//                    case "Green":
-//                        mpg.start();
-//                        break;
-//                    case "Yellow":
-//                        mpy.start();
-//                        break;
-//                    default:
-//                        // code block
-//                }
-////        private int checkCurrentSteam(ArrayList<String> first, ArrayList<String> clickStream){
-////            for (int i = 0 ; i < first.toArray().length; i++){
-////                if (first[i] != clickStream[i]){
-////                    return 0;
-////                }
-////                return 1;
-////            }
-//
-//            }//for
-//            level++;
-//        }//while
 
         AIe = 0;
         highScore = 20;
@@ -145,13 +76,27 @@ public class GameActivity extends AppCompatActivity {
 
 
 
-        blueBtn.setOnClickListener(new View.OnClickListener() {
+        blueAnim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mpb.start();
-                flashBlue();
+                animationDrawable.start();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        animationDrawable.stop();
+                    }
+                }, 1000);
             }
         });
+
+//        blueBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mpb.start();
+//                flashBlue();
+//            }
+//        });
 
         redBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -181,15 +126,17 @@ public class GameActivity extends AppCompatActivity {
     private void flashSequence() {
         playerAnswers = new ArrayList<>();
         Random rand = new Random();
-        sequence.add(rand.nextInt(3));
+//        sequence.add(rand.nextInt(3));
+        sequence.add(3);
+        sequence.add(0);
         for (int i = 0; i < roundNumber; i++) {
             int currentLight = sequence.get(i);
             AI = sequence;
             //should activate a flash for button by numbers finish the functions
-//            if(currentLight === 0) flashGreen();
+            if(currentLight == 0) flashGreen();
 //            if(currentLight === 1) flashRed();
 //            if(currentLight === 2) flashYellow();
-//            if(currentLight === 3) flashBlue();
+            if(currentLight == 3) flashBlue();
 
         }
     }
@@ -235,20 +182,45 @@ public class GameActivity extends AppCompatActivity {
 
     private void flashGreen(){
         //
+        final MediaPlayer mpg = MediaPlayer.create(this,R.raw.alien_blast_in_the_earth);
+        mpg.start();
+        AnimationDrawable animationDrawable = (AnimationDrawable) blueAnim.getDrawable();
+        animationDrawable.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animationDrawable.stop();
+            }
+        }, 1000);
         playerAnswers.add(0);
     }
 
     private void flashRed(){
+        final MediaPlayer mpr = MediaPlayer.create(this,R.raw.alien_blast_in_the_earth);
+        mpr.start();
         playerAnswers.add(1);
     }
 
     private void flashYellow(){
+        final MediaPlayer mpy = MediaPlayer.create(this,R.raw.alien_blast_in_the_earth);
+        mpy.start();
         playerAnswers.add(2);
     }
 
     private void flashBlue(){
+        final MediaPlayer mpb = MediaPlayer.create(this,R.raw.alien_blast_in_the_earth);
+        mpb.start();
+        AnimationDrawable animationDrawable = (AnimationDrawable) blueAnim.getDrawable();
+        animationDrawable.start();
+        blueAnim.setEnabled(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animationDrawable.stop();
+            }
+        }, 1000);
+        blueAnim.setEnabled(true);
         playerAnswers.add(3);
-
     }
 
     private void wait(int ms){
